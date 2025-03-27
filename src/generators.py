07aa5f6e2d95ae148df24d8a45159550534d2181
alestary <ivanovaaleksandra2259@gmail.com>
2025-03-27 13:33:24 +0300

    initisl

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,5 @@
-from typing import Iterator
+from typing import Generator, Iterator
+
 
 def filter_by_currency(lst: list[dict], currency: str = "USD") -> Iterator[dict]:
     """Генератор, который возвращает транзакции, где валюта операции соответствует заданной"""
@@ -6,57 +7,36 @@ def filter_by_currency(lst: list[dict], currency: str = "USD") -> Iterator[dict]
         if transaction["operationAmount"]["currency"]["name"] == currency:
             yield transaction
 
+
 transactions = [
     {
         "id": 939719570,
         "state": "EXECUTED",
         "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
+        "to": "Счет 11776614605963066702",
     },
     {
         "id": 142264268,
         "state": "EXECUTED",
         "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод со счета на счет",
         "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188"
+        "to": "Счет 75651667383060284188",
     },
     {
         "id": 873908921,
         "state": "EXECUTED",
         "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {
-            "amount": "43318.34",
-            "currency": {
-                "name": "RUB",
-                "code": "RUB"
-            }
-        },
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "RUB", "code": "RUB"}},
         "description": "Перевод с карты на карту",
         "from": "Maestro 4598300720424501",
-        "to": "Счет 43597928997568165086"
-    }
+        "to": "Счет 43597928997568165086",
+    },
 ]
-
-usd_transactions = filter_by_currency(transactions, "USD")
-for _ in range(2):
-    print(next(usd_transactions))
 
 
 def transaction_descriptions(lst: list[dict]) -> Iterator[dict]:
@@ -64,14 +44,14 @@ def transaction_descriptions(lst: list[dict]) -> Iterator[dict]:
     for transaction in lst:
         yield transaction["description"]
 
-descriptions = transaction_descriptions(transactions)
-for _ in range(3):
-    print(next(descriptions))
 
-def card_number_generator(start: int, stop: int) -> Iterator[dict]:
+def card_number_generator(start: int, stop: int) -> Generator:
     """Генератор, который выдает номера банковских карт в формате XXXX XXXX XXXX XXXX"""
     for number in range(start, stop + 1):
-        yield f"{number:016}"[:4] + " " + f"{number:016}"[4:8] + " " + f"{number:016}"[8:12] + " " + f"{number:016}"[12:]
+        yield (
+            f"{number:016}"[:4] + " " + f"{number:016}"[4:8] + " " + f"{number:016}"[8:12] + " " f"{number:016}"[12:]
+        )
+
 
 for card_number in card_number_generator(1, 5):
     print(card_number)
--- tests/confest.py
+++ tests/confest.py
@@ -11,3 +11,76 @@ def test_data() -> List:
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
+
+
+@pytest.fixture
+def card_number() -> str:
+    """
+    Фикстура, возвращающая тестовый номер карты в виде строки
+    """
+    return "1234567812345678"
+
+
+@pytest.fixture
+def account_number() -> str:
+    """
+    Фикстура, возвращающая тестовый номер счета в виде строки
+    """
+    return "1234567890"
+
+
+@pytest.fixture
+def card_number_input() -> str:
+    """
+    Фикстура, возвращающая тестовый номер карты с описанием в виде строки
+    """
+    return "Visa Platinum 7000792289606361"
+
+
+@pytest.fixture
+def account_number_input() -> str:
+    """
+    Фикстура, возвращающая тестовый номер счета с описанием в виде строки
+    """
+    return "Счет 73654108430135874305"
+
+
+@pytest.fixture
+def date_string_input() -> str:
+    """
+    Фикстура, возвращающая тестовую дату в виде строки
+    """
+    return "2024-03-11T02:26:18.671407"
+
+
+@pytest.fixture
+def transactions() -> list[dict]:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+        {
+            "id": 873908921,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "RUB", "code": "RUB"}},
+            "description": "Перевод с карты на карту",
+            "from": "Maestro 4598300720424501",
+            "to": "Счет 43597928997568165086",
+        },
+    ]
