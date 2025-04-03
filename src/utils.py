36bcf667bd41a8e98454d0d9018cbd67b7bdc98c
alestary <ivanovaaleksandra2259@gmail.com>
2025-04-03 13:04:58 +0300

    upload README.md file

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,15 @@
 import json
 from typing import Optional
+import logging
+
+logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler("logs/utils.log",  encoding="utf-8")
+
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+from flake8.main.debug import information
 
 
 def get_data(path) -> list[Optional[None, dict]]:
@@ -8,10 +18,14 @@ def get_data(path) -> list[Optional[None, dict]]:
     """
     try:
         with open(path, encoding="utf-8") as f:
+            logger.info("Загружает данные из JSON-файла и выводит в виде списка")
             data = json.load(f)
         if data:
+            logger.info("Выводит JSON-файл")
             return data
         else:
+            logger.debug("Выводит пустой список")
             return []
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("Файл не найден")
         return []
